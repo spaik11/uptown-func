0c94dc630b58c58205f9c48f6e53099dfd90d1df
Sung Paik <sung.paik@codeimmersives.com>
2019-10-28 18:21:26 -0400

    completed exercise

--- main.js
+++ main.js
@@ -1,31 +1,46 @@
-function call() {
+function call(return5) {
+  return5();
 }
 
-function callTwice() {
+function callTwice(return5) {
+  call(return5);
+  call(return5);
 }
 
-function callXTimes() {
+function callXTimes(func, callTimes) {
+  let count = 0;
+  while (count < callTimes) {
+    func();
+    count++;
+  }
 }
 
-function returnFromFunc() {
+function returnFromFunc(return5) {
+  return return5();
 }
 
-function modifyString() {
+function modifyString(str, func) {
+  return func(str);
 }
 
-function modifyNumber() {
+function modifyNumber(num, func) {
+  return func(num);
 }
 
-function modifyAnything() {
+function modifyAnything(anything, func) {
+  return func(anything);
 }
 
-function twoFuncs() {
+function twoFuncs(sayHello, addQuestionMark) {
+  return addQuestionMark(sayHello());
 }
 
-function twoValues() {
+function twoValues(val1, val2, func) {
+  return func(val1, val2);
 }
 
-function twoValuesRTL() {
+function twoValuesRTL(val1, val2, func) {
+  return func(val2, val1);
 }
 
 
